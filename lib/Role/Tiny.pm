b25f06bb6246c83c4e010e41a7e4b22b11afe65f
Graham Knop <haarg@haarg.org>
2019-09-11 18:24:38 +0200

    Bumping version to 2.001_000

--- lib/Role/Tiny.pm
+++ lib/Role/Tiny.pm
@@ -2,7 +2,7 @@ package Role::Tiny;
 use strict;
 use warnings;
 
-our $VERSION = '2.000_009';
+our $VERSION = '2.001_000';
 $VERSION =~ tr/_//d;
 
 our %INFO;
--- lib/Role/Tiny/With.pm
+++ lib/Role/Tiny/With.pm
@@ -3,7 +3,7 @@ package Role::Tiny::With;
 use strict;
 use warnings;
 
-our $VERSION = '2.000_009';
+our $VERSION = '2.001_000';
 $VERSION =~ tr/_//d;
 
 use Role::Tiny ();
